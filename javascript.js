1357cec1c430b0fe460b937ea2a52021780c9269
Laurens Vanderstraeten <laurensvanderstraeten@gmail.com>
2022-12-31 23:53:25 +0100

    Implement the playRound method

--- javascript.js
+++ javascript.js
@@ -1,3 +1,38 @@
+function playRound(playerSelection, computerSelection) {
+    formattedPlayerSelection = formatPlayerSelection(playerSelection);
+    if (!formattedPlayerSelection) {
+        return "Please enter either Rock, Paper or Scissors";
+    }
+    if (formattedPlayerSelection == computerSelection) {
+        return `Tie! You both chose ${computerSelection}!`
+    }
+    let didPlayerWin;
+    if (formattedPlayerSelection == "Rock") {
+        didPlayerWin = computerSelection == "Scissors";
+    } else if (formattedPlayerSelection == "Paper") {
+        didPlayerWin = computerSelection == "Rock";
+    } else {
+        didPlayerWin = computerSelection == "Paper";
+    }
+    if (didPlayerWin) {
+        return `You won! ${formattedPlayerSelection} beats ${computerSelection}!`
+    }
+    return `You lost! ${computerSelection} beats ${formattedPlayerSelection}!`
+}
+
+function formatPlayerSelection(playerSelection) {
+    if (!playerSelection || playerSelection.length < 3) {
+        return null;
+    }
+    let formattedPlayerSelection = playerSelection.trim().toLowerCase();
+    formattedPlayerSelection = formattedPlayerSelection.charAt(0).toUpperCase()
+        + formattedPlayerSelection.slice(1);
+    if (formattedPlayerSelection != "Rock" && formattedPlayerSelection != "Paper" && formattedPlayerSelection != "Scissors") {
+        return null;
+    }
+    return formattedPlayerSelection;
+}
+
 function getComputerChoice() {
     let computerChoice;
     switch (getRandomInt(3)) {
